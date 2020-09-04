cd0aa428d60677fbf1be253c8e3ec3b8bb552c39
ultreson <alexitessier@hotmail.com>
2020-09-04 16:18:33 -0400

    added remember passphrase script which just executes ssh add

---
+++ remember-ssh-passphrase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Author : https://github.com/ultreson
+# Creation Date : 2017-11-29
+# Source : github-archive repository https://github.com/Justintime50/github-archive
+# Tested on : Raspberry Pi OS with bash 5.0.3
+
+ssh-add
