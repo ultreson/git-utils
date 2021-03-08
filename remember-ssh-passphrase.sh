94548575a4624d2f168cc25d8ce56fdbc8147ba7
ultreson <ultreson@outlook.com>
2021-03-08 18:59:12 -0500

    Update remember-ssh-passphrase.sh

--- remember-ssh-passphrase.sh
+++ remember-ssh-passphrase.sh
@@ -3,5 +3,6 @@
 # Creation Date : 2017-11-29
 # Source : github-archive repository https://github.com/Justintime50/github-archive
 # Tested on : Raspberry Pi OS with bash 5.0.3
+# See better version of this script under https://github.com/ultreson/scripts/blob/master/adding-ssh-key.sh (https://github.com/ultreson/scripts/blob/958c3f23e852604d7a9a8105c6167c17898acd00/adding-ssh-key.sh)
 
 ssh-add
